7e4152496fc05773e67978379acf2eb122cf7447
shaleen kacker <shaleenkacker@gmail.com>
2016-06-28 00:18:54 -0400

    integrated flink dependencies.

--- main/com/rand/rest/RestService.java
+++ main/com/rand/rest/RestService.java
@@ -1,9 +1,14 @@
 package com.rand.rest;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplications
 public class RestService {
 
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		SpringApplication.run(RestService.class, args);
 
 	}
 
